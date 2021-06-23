6df5beeeaf0126f6e3d4ca4f5c8f1e2de35bc59e
GakPower <alanigogos@gmail.com>
2021-06-23 16:12:17 +0200

    Added API utils for getPokemon route

---
+++ src/pokeAPI_Utils.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const getPokemon = async (limit?: number, offset?: number) => {
+	const { error, next, results } = (await axios.post('/api/getPokemon', { limit, offset })).data;
+
+	if (error) return {};
+
+	if (!next) {
+		return { next: null, results };
+	}
+
+	const url = new URL(next);
+	const nextLimit = url.searchParams.get('limit');
+	const nextOffset = url.searchParams.get('offset');
+	return { next: { limit: nextLimit, offset: nextOffset }, results };
+};
