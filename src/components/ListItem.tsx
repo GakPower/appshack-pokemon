69ed8f7ca31bc77268d7286000dcf03a7366f6c8
GakPower <alanigogos@gmail.com>
2021-06-24 16:25:47 +0200

    Created a ListItem component for pokemon basic data

---
+++ src/components/ListItem.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from '../../styles/ListItem.module.scss';
+
+type PropType = {
+	name: string,
+	picture: string
+	abilities: string[]
+}
+
+export default function ListItem({ name, picture, abilities }: PropType) {
+	return (
+		<div className={styles.container}>
+			<img className={styles.picture} src={picture} alt={`Pokemon named ${name}`} />
+			<ul>
+				<li>Name: <span>{name}</span></li>
+				{abilities.map(ability => <li key={`${name}_${ability}`}>{ability}</li>)}
+			</ul>
+		</div>
+	);
+}
