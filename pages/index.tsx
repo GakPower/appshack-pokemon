528175ad4a390ecfed0cf4afb4fcc4ed65539e4c
GakPower <alanigogos@gmail.com>
2021-06-24 18:27:46 +0200

    Added pagination options on home list view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,30 @@
-import { useEffect, useState } from 'react';
 import { getPokemon } from '../src/pokeAPI_Utils';
 import styles from '../styles/Home.module.scss';
 import ListItem from './../src/components/ListItem';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
 	const [pokemonArray, setPokemonArray] = useState([]);
+	const [currentPage, setCurrentPage] = useState(0);
+	const [totalPages, setTotalPages] = useState(0);
 
 	useEffect(() => {
-		getPokemon().then((data) => {
+		getPokemon(currentPage).then((data) => {
 			setPokemonArray(data.results);
+			setTotalPages(data.count / 10);
 		});
-	}, []);
+	}, [currentPage]);
 	return (
 		<>
 			<div className={styles.list}>
 				{pokemonArray.map((pokemon: any) => (<ListItem key={pokemon.name} name={pokemon.name} picture={pokemon.picture} abilities={pokemon.abilities} />))}
+			</div>
+			<div className={styles.pageDiv}>
+				{
+					Array.from({ length: totalPages }, (_, k) => (
+						<button onClick={() => setCurrentPage(k)}>{k}</button>
+					))
+				}
 			</div>
 		</>
 	);
