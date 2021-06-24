dcbf5dba56960f5c264b503aff8a0d4a2b52d521
GakPower <alanigogos@gmail.com>
2021-06-24 16:28:16 +0200

    Created the main UI for list view of pokemon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react';
+import { getPokemon } from '../src/pokeAPI_Utils';
 import styles from '../styles/Home.module.scss';
+import ListItem from './../src/components/ListItem';
 
 export default function Home() {
+	const [pokemonArray, setPokemonArray] = useState([]);
+
+	useEffect(() => {
+		getPokemon().then((data) => {
+			setPokemonArray(data.results);
+		});
+	}, []);
 	return (
 		<>
-
+			<div className={styles.list}>
+				{pokemonArray.map((pokemon: any) => (<ListItem key={pokemon.name} name={pokemon.name} picture={pokemon.picture} abilities={pokemon.abilities} />))}
+			</div>
 		</>
 	);
 }
