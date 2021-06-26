806f70077c2bc462bcf0720e1093520f3100da7e
GakPower <alanigogos@gmail.com>
2021-06-27 01:51:58 +0200

    Added type checking and improved code readability on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,31 +3,28 @@ import styles from '../styles/Home.module.scss';
 import ListItem from './../src/components/ListItem';
 import { useEffect, useState } from 'react';
 import Loading from './../src/components/Loading';
+import { BasicPokemon } from '../src/types';
 
 const PAGE_SIZE = 10;
 let offset = 0;
-let pokemonArray: any[] = [];
+let pokemonArray: { name: string }[] = [];
 
 export default function Home() {
-	const [currentPage, setCurrentPage] = useState([]);
-	const [currentPageNumber, setCurrentPageNumber] = useState(-1);
+	const [currentPage, setCurrentPage] = useState<BasicPokemon[]>([]);
+	const [currentPageNumber, setCurrentPageNumber] = useState(0);
 	const [totalPages, setTotalPages] = useState(0);
 	const [loading, setLoading] = useState(false);
 	const [sortingOption, setSortingOption] = useState(false);
 
 	useEffect(() => {
 		getAllPokemon().then((data) => {
-			if (data.results) {
-				const sortedData = (data.results.sort((a: any, b: any) => {
-					if (sortingOption) {
-						return a.name.localeCompare(b.name);
-					} else {
-						return b.name.localeCompare(a.name);
-					}
-				}));
-				pokemonArray = sortedData;
-				setTotalPages(Math.ceil(data?.count / PAGE_SIZE));
-				setCurrentPageNumber(0);
+			if (data.length) {
+				setLoading(true);
+
+				pokemonArray = data;
+				pokemonArray = getSortedPokemon(false);
+				setTotalPages(Math.ceil(data.length / PAGE_SIZE));
+				updateCurrentPage();
 			}
 		});
 	}, []);
@@ -36,48 +33,51 @@ export default function Home() {
 		setLoading(true);
 
 		offset = PAGE_SIZE * currentPageNumber;
-		const newPage = pokemonArray
-			.slice(offset, offset + PAGE_SIZE)
-			.map(async (pokemon: any) => {
-				return await getBasicPokemon(pokemon.name);
-			});
-
-		Promise.all(newPage).then((res: any) => {
-			setCurrentPage(res);
-			setLoading(false);
-		});
+		updateCurrentPage();
 	}, [currentPageNumber]);
 
 	useEffect(() => {
 		setLoading(true);
-		const temp = pokemonArray.sort((a: any, b: any) => {
-			if (sortingOption) {
+
+		pokemonArray = getSortedPokemon(sortingOption);
+		updateCurrentPage();
+	}, [sortingOption]);
+
+	const getSortedPokemon = (option: boolean) => {
+		return pokemonArray.sort((a, b) => {
+			if (option) {
 				return a.name.localeCompare(b.name);
 			} else {
 				return b.name.localeCompare(a.name);
 			}
 		});
-		pokemonArray = temp;
-		const newPage = temp
+	};
+
+	const updateCurrentPage = () => {
+		const newPage = pokemonArray
 			.slice(offset, offset + PAGE_SIZE)
-			.map(async (pokemon: any) => {
+			.map(async (pokemon) => {
 				return await getBasicPokemon(pokemon.name);
 			});
 
-		Promise.all(newPage).then((res: any) => {
+		Promise.all(newPage).then((res) => {
 			setCurrentPage(res);
+
 			setLoading(false);
 		});
-	}, [sortingOption]);
+	};
 
 	return (
 		<>
-			<div className={styles.sortingOptions}><button onClick={() => setSortingOption(oldState => !oldState)}>Sort Alphabetically</button>{sortingOption ? 'DEC' : 'ASC'}</div>
-			{!loading ? (
+			{loading ? (
+				<Loading />
+			) : <>
+				<div className={styles.sortingOptions}><button onClick={() => setSortingOption(oldState => !oldState)}>Sort Alphabetically</button>{sortingOption ? 'DEC' : 'ASC'}</div>
 				<div className={styles.list}>
 					{currentPage.map((pokemon: any) => (<ListItem key={pokemon.name} data={pokemon} clickable />))}
 				</div>
-			) : <Loading />}
+			</>
+			}
 
 			<div className={styles.pageDiv}>
 				{
