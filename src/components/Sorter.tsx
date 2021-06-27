64a7921fd0589d66191d9f16a1d2c9f3af18e66b
GakPower <alanigogos@gmail.com>
2021-06-27 04:38:56 +0200

    Sorter now accept the value (option) that is selected as prop

--- src/components/Sorter.tsx
+++ src/components/Sorter.tsx
@@ -3,17 +3,18 @@ import styles from '../../styles/Sorter.module.scss';
 import { IoIosArrowDown } from 'react-icons/io';
 
 type PropType = {
+	value: boolean,
 	onChange: (isASC: boolean) => void,
 	loading: boolean
 }
 
-export default function Sorter({ onChange, loading }: PropType) {
+export default function Sorter({ value, onChange, loading }: PropType) {
 	const options = [
 		'A - Z',
 		'Z - A',
 	];
 	const [openSorter, setOpenSorter] = useState(false);
-	const [selectedSorter, setSelectedSorter] = useState<string>(options[0]);
+	const [selectedSorter, setSelectedSorter] = useState<string>(value ? options[0] : options[1]);
 	return (
 		<div
 			className={`${styles.sorter} ${openSorter && styles.open} ${loading && styles.loading}`}
