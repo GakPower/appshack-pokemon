eb4018053407c3982a02a1fcfb6672c878573e93
GakPower <alanigogos@gmail.com>
2021-06-28 04:26:15 +0200

    Added logo on top left of each page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,23 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Image from 'next/image';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<main>
 			<Head>
-				<title>AppShack PokeAPI Visualization</title>
+				<title>AppShack PokeAPI</title>
 				<meta name='description' content='A Front-end application that visualizes PokeAPI' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
 			<Component {...pageProps} />
+
+			<div style={{ position: 'fixed', top: 20, left: 20, display: 'flex', alignItems: 'center', border: '2px solid black', borderRadius: '15px', padding: '5px' }}>
+				<Image src='/favicon.ico' alt='Pokemon Icon' width={50} height={50} />
+				<h2 style={{ fontWeight: 'bold' }}>AppShack <span style={{ color: '#E64C3C' }}>Poke</span>API</h2>
+			</div>
 		</main>
 	);
 }
