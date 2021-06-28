fb7b1c6e03fc883342f9b980a100f934ae3df674
GakPower <alanigogos@gmail.com>
2021-06-28 05:52:32 +0200

    Added SearchBar to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Loading from './../src/components/Loading';
 import { BasicPokemon } from '../src/types';
 import Sorter from '../src/components/Sorter';
 import PageSelectionBar from '../src/components/PageSelectionBar';
+import SearchBar from '../src/components/SearchBar';
 
 const PAGE_SIZE = 10;
 let offset = 0;
@@ -17,6 +18,7 @@ export default function Home() {
 	const [totalPages, setTotalPages] = useState(0);
 	const [loading, setLoading] = useState(false);
 	const [sortingOption, setSortingOption] = useState(true);
+	const [searchValue, setSearchValue] = useState('');
 
 	useEffect(() => {
 		getAllPokemon().then((data) => {
@@ -44,6 +46,28 @@ export default function Home() {
 		pokemonArray = getSortedPokemon(sortingOption);
 		updateCurrentPage();
 	}, [sortingOption]);
+
+	useEffect(() => {
+		if (searchValue) {
+			setLoading(true);
+			const newPage = pokemonArray
+				.filter((element) => element.name.match(searchValue))
+				.slice(0, PAGE_SIZE)
+				.map(async (pokemon) => {
+					return await getBasicPokemon(pokemon.name);
+				});
+
+			Promise.all(newPage).then((res) => {
+				setCurrentPage(res);
+
+				setLoading(false);
+			});
+		} else {
+			setLoading(true);
+
+			updateCurrentPage();
+		}
+	}, [searchValue]);
 
 	const getSortedPokemon = (option: boolean) => {
 		return pokemonArray.sort((a, b) => {
@@ -73,11 +97,13 @@ export default function Home() {
 		<>
 			{loading ? <Loading /> : (
 				<>
-					<Sorter value={sortingOption} onChange={(isASC) => setSortingOption(isASC)} loading={!pokemonArray} />
+					{searchValue && !currentPage.length && !loading && <strong>There are no results</strong>}
+					<SearchBar value={searchValue} onChange={setSearchValue} />
 					<div className={styles.list}>
 						{currentPage.map((pokemon) => (<ListItem key={pokemon.name} data={pokemon} clickable />))}
 					</div>
-					<PageSelectionBar totalPages={totalPages} pageNumber={currentPageNumber} onChange={(pageNumber) => setCurrentPageNumber(pageNumber)} />
+					<Sorter value={sortingOption} onChange={(isASC) => setSortingOption(isASC)} loading={!pokemonArray} />
+					{!searchValue && <PageSelectionBar totalPages={totalPages} pageNumber={currentPageNumber} onChange={(pageNumber) => setCurrentPageNumber(pageNumber)} />}
 				</>
 			)}
 		</>
